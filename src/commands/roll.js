eb4d509d55418c78825e6d82f15774334447ffe9
Alex Butler <alexbutler@Don-Butlero.local>
2020-04-19 21:14:21 +0100

    Added comments to roll function

--- src/commands/roll.js
+++ src/commands/roll.js
@@ -3,6 +3,8 @@ const roll = {
     description: 'Randomised dice roller',
     execute(message, args) {
         dice_type = Number(args[0]);
+
+        // See if user has a modifier to their roll
         if (args.length > 1) {
             modifier = Number(args[1]);
         } else {
@@ -11,16 +13,19 @@ const roll = {
         // Roll the dice...
         const roll = Math.ceil(Math.random() * dice_type);
 
+        // For natural max of the decided die size
         if (roll == args[0]) {
             message.reply(`Wow, you rolled a natural ${args[0]}!!`)
             if (args.length > 1) {
                 message.reply(`With your modifier, that's ${roll + modifier}!`)
             }
+        // For bad luck
         } else if (roll == 1) {
             message.reply(`Uff, you rolled a natural 1`)
             if (args.length > 1) {
                 message.reply(`With your modifier, that's ${roll + modifier}, sorry!`)
             }
+        // For everything else
         } else {
             message.reply(`You rolled ${roll + modifier}`)
         }
