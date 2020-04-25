5532e128ce91e359dfc711cd4db6757f3033f5b9
Alex Butler <alexbutler@Don-Butlero.local>
2020-04-25 16:37:24 +0100

    Adjusted inputs to read from more natural speach

--- src/commands/roll.js
+++ src/commands/roll.js
@@ -1,33 +1,61 @@
+const modder = (message, roll, mod) => {
+    var total = roll + mod;
+    if (mod != 0) {
+        message.reply(`With your modifier, that's ${total}`);
+    }
+    return total;
+}
+
+const roller = (message, dice_type, modifier) => {
+    let roll = Math.ceil(Math.random() * dice_type);
+    if (roll == dice_type) {
+        message.reply(`Wow, you rolled a natural ${dice_type}!!`);
+        var score = modder(message, roll, modifier);
+    // For bad luck
+    } else if (roll == 1) {
+        message.reply(`Uff, you rolled a natural 1`);
+        var score = modder(message, roll, modifier);
+    // For everything else
+    } else {
+        message.reply(`You rolled ${roll}`);
+        var score = modder(message, roll, modifier);
+    }
+    return score;
+}
+
+
 const roll = {
     name: 'roll',
     description: 'Randomised dice roller',
     execute(message, args) {
-        dice_type = Number(args[0]);
-
+        try {
+            dice_count = Number(args[0]);
+        } catch (e) {
+            dice_count = 1; // makes 'a' a valid input for a single die
+        }
+        
+        dice_type = Number(args[1].replace(/\D/g, ''));
         // See if user has a modifier to their roll
-        if (args.length > 1) {
-            modifier = Number(args[1]);
+        if (args.length > 2) {
+            modifier = Number(args[args.length - 1]);
+            if (isNaN(modifier)) {
+                modifier = Number(args[args.length - 2]);
+            }  // allows users more english like sytanc
         } else {
             modifier = 0;
         }
         // Roll the dice...
-        const roll = Math.ceil(Math.random() * dice_type);
-        // For natural max of the decided die size
-        if (roll == args[0]) {
-            message.reply(`Wow, you rolled a natural ${args[0]}!!`)
-            if (args.length > 1) {
-                message.reply(`With your modifier, that's ${roll + modifier}!`)
+        if (dice_count > 1) {
+            var total = 0
+            for (i = 1; i <= dice_count; i++) {
+                message.reply(`For die number ${i}...`);
+                total += roller(message, dice_type, modifier);
             }
-        // For bad luck
-        } else if (roll == 1) {
-            message.reply(`Uff, you rolled a natural 1`)
-            if (args.length > 1) {
-                message.reply(`With your modifier, that's ${roll + modifier}, sorry!`)
-            }
-        // For everything else
+            message.reply(`So your total score is ${total}`);
         } else {
-            message.reply(`You rolled ${roll + modifier}`)
+            roller(message, dice_type, modifier);
         }
+        
     }
 }
 
--- src/commands/roll_for.js
+++ src/commands/roll_for.js
@@ -1,34 +1,65 @@
+const modder = (message, roll, mod) => {
+  var total = roll + mod;
+  if (mod != 0) {
+      message.channel.send(`With their modifier, that's ${total}`);
+  }
+  return total;
+}
+
+const roller = (message, opponent, dice_type, modifier) => {
+  let roll = Math.ceil(Math.random() * dice_type);
+  if (roll == dice_type) {
+      message.channel.send(`Uff, ${opponent} rolled a natural ${dice_type}!!`);
+      var score = modder(message, roll, modifier);
+  // For bad luck
+  } else if (roll == 1) {
+      message.channel.send(`Woo, ${opponent} rolled a natural 1`);
+      var score = modder(message, roll, modifier);
+  // For everything else
+  } else {
+      message.channel.send(`${opponent} rolled ${roll}`);
+      var score = modder(message, roll, modifier);
+  }
+  return score;
+}
+
+
 const roll = {
   name: 'roll_for',
   description: 'Randomised dice roller for NPCs',
   execute(message, args) {
-    dice_type = Number(args[1]);
+    const opponent = args[0];
+    let dice_count = Number(args[1]);
+    if (isNaN(dice_count)) {
+      dice_count = 1;
+    }
 
+    const dice_type = Number(args[2].replace(/\D/g, ''));
     // See if user has a modifier to their roll
-    if (args.length > 2) {
-      modifier = Number(args[2]);
+    if (args.length > 3) {
+        modifier = Number(args[args.length - 1]);
+        if (isNaN(modifier)) {
+            modifier = Number(args[args.length - 2]);
+        }  // allows users more english like sytanc
     } else {
-      modifier = 0;
+        modifier = 0;
     }
-    // Roll the dice...
-    const roll = Math.ceil(Math.random() * dice_type);
-    // For natural max of the decided die size
-    if (roll == args[0]) {
-      message.channel.send(`Uff, ${args[0]} rolled a natural ${args[1]}!!`);
-      if (args.length > 1) {
-        message.channel.send(`With their modifier, that's ${roll + modifier}!`);
+
+     // Roll the dice...
+    if (dice_count > 1) {
+      var total = 0
+      for (i = 1; i <= dice_count; i++) {
+          message.channel.send(`For die number ${i}...`);
+          total += roller(message, opponent, dice_type, modifier);
       }
-      // For bad luck
-    } else if (roll == 1) {
-      message.channel.send(`Woo, ${args[0]} rolled a natural 1`);
-      if (args.length > 1) {
-        message.channel.send(`With their modifier, that's ${roll + modifier}!`);
-      }
-      // For everything else
+      message.channel.send(`So ${opponent}'s total score is ${total}`);
     } else {
-      message.channel.send(`${args[0]} rolled ${roll + modifier}`);
-    }
-  },
+        roller(message, opponent, dice_type, modifier);
+  }
+
+  }
 };
 
 module.exports = roll;
+
+
